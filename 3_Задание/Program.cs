469fcbc151a25f9008f37c2fcf708c36c6ec51b5
Cenbka <108518693+c3n9@users.noreply.github.com>
2022-10-26 15:30:40 +0300

    Update Program.cs

--- 3_Задание/Program.cs
+++ 3_Задание/Program.cs
@@ -11,28 +11,20 @@ namespace _3_Задание
         static void Main(string[] args)
         {
             
-            try
+             try
             {
-                try
-                {
                     Console.Write("Введите число №1 = ");
-                    int a = Convert.ToInt32(Console.ReadLine());
+                    uint a = Convert.ToUInt32(Console.ReadLine());
                     Console.Write("Введите число №2 = ");
-                    int b = Convert.ToInt32(Console.ReadLine());
+                    uint b = Convert.ToUInt32(Console.ReadLine());
                     Console.WriteLine($"{a}+{b}={a + b}");
                     Console.WriteLine($"{a}-{b}={a - b}");
                     Console.WriteLine($"{a}*{b}={a * b}");
                     Console.WriteLine($"{a}/{b}={a / b}");
-                }
-                catch when (a<0)
-                {
-                    Console.WriteLine("22");
-                }
-              
             }
             catch
             {
-                Console.WriteLine("Неверный формат ввода!");
+                    Console.WriteLine("Число не может быть отрицательным");
             }
         }
     }
